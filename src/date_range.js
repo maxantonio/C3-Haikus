cf830c880a6bccb5db2b2e49a0f77604c1327b44
Example <dayrobis@jovenclub.cu>
2015-05-04 19:56:17 -0400

    implementado todos los periodos y validacion de cuando los datos no estan en el intervalo de los datos

--- src/date_range.js
+++ src/date_range.js
@@ -1,61 +1,55 @@
-////test
-//var tiempo = ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'];
-//tiempo = tiempo.slice(1, tiempo.length);
-//
-////Selecciona el periodo
-//function todo() {
-//    chart.axis.min({x: tiempo[0]});
-//};
-//
-//function tresdias() {
-//    chart.axis.min({x: tiempo[tiempo.length - 3]});
-//};
+var StockTools = function (raiz, periodos) {
+    'use strict';
 
-
-var M = function (raiz, periodos) {
-
+    //Crea los componentes
     init();
 
     function init() {
+
         var header = d3.select(raiz).append("div")
             .attr("class", "c3-header");
 
+        // Crea el contenedor de los periodos
         var p = header.append("div")
             .attr('class', 'c3-periodos')
             .style('float', 'left');
 
+        // Crea el contenedor de los intervalos (input para las fechas)
         var intervalos = header.append("div")
             .attr('class', 'c3-intervalos')
             .style('float', 'left');
 
+        // Crea el texto y el input para la fecha inicial
         intervalos.append("span").text("Desde: ");
-
         intervalos.append("input")
             .attr("type", "text")
             .attr("name", "inicio")
             .attr("id", "inicio");
 
+        // Crea el texto y el input para la fecha final
         intervalos.append("span").text("Hasta: ");
         intervalos.append("input")
             .attr("type", "text")
             .attr("name", "fin")
             .attr("id", "fin");
 
-
+        // Div contenedor de los botones
         var botones = header.append("div")
             .attr('class', 'c3-botones')
             .style('float', 'left');
 
+        // Boton Actualizar
         botones.append("button")
             .attr("id", "update")
             .attr("type", "button")
-            .text("Update")
+            .text("Actualizar")
             .on('click', update_click);
 
         header.append("div")
             .attr('class', 'c3-clear-header')
             .style('clear', 'both');
 
+        // Crea todos los botones de los periodos
         p.selectAll("botones")
             .data(periodos)
             .enter()
@@ -75,26 +69,47 @@ var M = function (raiz, periodos) {
             }).on('click', click_periodo);
 
 
+        //Evento click  en un boton del periodo
         function click_periodo() {
 
+            //Valor del periodo clickeado
             var cantidad = d3.select(this).attr("data-value");
+
+            //Tipo de periodo
             var tipo = d3.select(this).attr("data-tipo");
 
+            //Ultima fecha de los datos a graficar
             var posLastDate = datos.columns[0].length - 1;
-            var lastDate = parseDate(datos.columns[0][posLastDate]);
-            var fechaInicio = null, fechaFin = null;
 
-            if (tipo == "dia") {
-                fechaInicio = new Date(lastDate.getTime());
-                fechaInicio.setDate(lastDate.getDate() - cantidad);
-                fechaFin = lastDate;
-                document.getElementById("inicio").value = formatDate(fechaInicio);
-                document.getElementById("fin").value = formatDate(fechaFin);
+            var fechaFin = parseDate(datos.columns[0][posLastDate]);
+            var fechaInicio = new Date(fechaFin.getTime());
 
+            // Inicio y fin de los datos
+            var inicioDatos = parseDate(datos.columns[0][1]);
+            var finDatos = fechaFin;
+
+            switch (tipo) {
+                case "dia":
+                    fechaInicio.setDate(fechaFin.getDate() - cantidad);
+                    break;
+                case "mes":
+                    fechaInicio.setMonth(fechaFin.getMonth() - cantidad);
+                    break;
+                case "anno":
+                    fechaInicio.setYear(fechaFin.getFullYear() - cantidad); //Le resta 2 años
+                    break;
+                case "hasta_la_fecha":
+                    fechaInicio = new Date(fechaFin.getFullYear(), 0, 1);
+                    break;
             }
 
-            updateGrafica(fechaInicio, fechaFin);
-
+            if (Intervalo_Correcto(inicioDatos, finDatos, fechaInicio, fechaFin)) {
+                updateGrafica(fechaInicio, fechaFin);
+                document.getElementById("inicio").value = formatDate(fechaInicio);
+                document.getElementById("fin").value = formatDate(fechaFin);
+            }
+            else
+                throw new Error("No hay datos para este intervalo");
         }
 
         function updateGrafica(fechaInicio, fechafin) {
@@ -107,7 +122,11 @@ var M = function (raiz, periodos) {
             var fechaFin = parseDate(document.getElementById("fin").value);
             updateGrafica(fechaInicio, fechaFin);
         }
-    }
 
+        //Dev true si las fechas estan en el intervalo de los datos
+        function Intervalo_Correcto(inicioDatos, finDatos, fechaInicio, fechaFin) {
+            return fechaInicio >= inicioDatos && fechaFin <= finDatos;
+        }
+    }
 
 };
