ca10130c1d6f6ebcaf9a4bec0fff62d95d56a7f6
yoidel86 <yoidel86@gmail.com>
2015-05-15 09:40:11 -0400

    graficos casados sin ciclo infinito

--- src/IRStrat.Donuts.js
+++ src/IRStrat.Donuts.js
@@ -4,9 +4,7 @@
 IDonuts = function(source,config ) {
     'use strict';
     var valores = config.values;
-    //actual simbolo que se esta comparando
-    var current_selected_value = "";
-    var ids = [];
+
 
     //Crea los componentes
     init();
@@ -29,27 +27,25 @@ IDonuts = function(source,config ) {
                 .attr("class","title")
                 .append("text")
                 .text(config.title)
-                .style("margin-left",function(d){
+                .style("margin-left",function(){
                      return ((width / 2)-(config.title.length*5))+"px"
                  }).style("color","rgb(120,120,121);");
-
-            ;
-
+        //agrego titulo del grafico si se pasa por configuracion
         if(config.subtitle)
-            header.append("div").attr("class","subtitle").append("text").text(config.subtitle).style("margin-left", function(d){
+            header.append("div").attr("class","subtitle").append("text").text(config.subtitle).style("margin-left", function(){
                return ((width / 2)-(config.subtitle.length*5))+"px";
             });
         //seteo valor por defecto tamaño grafico
 
 //
-        var outerRadius = height/2 , innerRadius = outerRadius -40, cornerRadius = 100;
+        var outerRadius = height/2 , innerRadius = outerRadius -40;
         var outerRadiuss = [outerRadius,outerRadius-25,outerRadius-50] ;
         var innerRadiuss = [innerRadius,innerRadius-25,innerRadius-50] ;
 
         var svg = header.append("div").append("svg").attr("width", width).attr("height", height)
-            .append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            .append("g").attr("transform", "translate(" + width / 2 + "," + ((height / 2)-13) + ")");
 
-        var arcs = new Array();
+        var arcs = [];
         var pie = d3.layout.customPie();
 
     //      agrego div para tooltip
@@ -75,16 +71,16 @@ for (var i=0; i < valores.length; i++){
 
     g.append("path")
         .each(function(d) {
-            d.outerRadius = outerRadiuss[i] - 20;
+            d.outerRadius = outerRadiuss[i] - 23;
         })
         .attr("d", arcs[i])
-        .attr("class",function(d,j){
+        .attr("class",function(){
             return "arc0"+i;
         }).on('mouseover', function(d){
             tooltip.select('.label').html(d.key);
             tooltip.select('.count').html(d.value+ '%');
             tooltip.style('display', 'block');
-        }).on('mousemove', function(d) {
+        }).on('mousemove', function() {
         tooltip.style('top', (d3.event.pageY + 10) + 'px')
             .style('left', (d3.event.pageX + 10) + 'px');
     }).on('mouseout', function() {
@@ -92,11 +88,12 @@ for (var i=0; i < valores.length; i++){
         });
 
     g.append("text")
-        .attr("transform", function(d) { return "translate(-25,"+(-(outerRadius-30)+i*25)+")"; })
+        .attr("transform", function() { return "translate(-30,"+(-(outerRadius-30)+i*25)+")"; })
         .attr("dy", ".35em")
         .style("text-anchor", "middle")
         .style("foreground","red")
         .attr("class","textValue")
         .text(valores[i].value+"%");
 }
-    }}
+    }
+}
